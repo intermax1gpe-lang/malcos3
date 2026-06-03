4f63e84a60f895ac11d152ba054b0f7da3e9699d
intermax1gpe-lang <intermax1gpe@gmail.com>
2026-06-02 19:17:38 -0500

    feat: agregar catalogos YUSPHONE, BELCOM y KOCOM con componente reutilizable

---
+++ src/data/belcomProducts.ts
@@ -0,0 +1,21 @@
+import type { ProductCategory } from './commaxProducts';
+
+export const belcomProducts: ProductCategory[] = [
+  {
+    category: 'Línea Casa',
+    slug: 'linea-casa',
+    products: [
+      { name: 'Intercomunicador BDP-202', img: 'https://malcos.com.pe/wp-content/uploads/2024/01/Productos-Belcom-Intercomunicador-BDP-202-ok1-300x300.png', url: 'https://malcos.com.pe/producto/intercomunicador-bdp-202/' },
+      { name: 'Intercomunicador BIDP-205', img: 'https://malcos.com.pe/wp-content/uploads/2024/01/BELCOM-INTERCOMUNICADOR-BIDP-205-1-300x300.png', url: 'https://malcos.com.pe/producto/intercomunicador-bidp-205/' },
+    ],
+  },
+  {
+    category: 'Línea Edificio',
+    slug: 'linea-edificio',
+    products: [
+      { name: 'Intercomunicador BDP-202 (Edificio)', img: 'https://malcos.com.pe/wp-content/uploads/2024/01/BDP-202-4-300x300.png', url: 'https://malcos.com.pe/producto/intercomunicador-bdp-202-2/' },
+      { name: 'Portero BDD-2L3M', img: 'https://malcos.com.pe/wp-content/uploads/2024/01/Belcom-Productos-Linea-edificio-audio-portero-edificio-Cod-BDD-2L3M-01-300x300.png', url: 'https://malcos.com.pe/producto/portero-bdd-2l3m/' },
+      { name: 'Portero BDD-4L5M', img: 'https://malcos.com.pe/wp-content/uploads/2024/01/Belcom-Productos-Linea-edificio-audio-portero-edificio-Cod-BDD-4L5M-02-300x300.png', url: 'https://malcos.com.pe/producto/portero-bdd-4l5m/' },
+    ],
+  },
+];
